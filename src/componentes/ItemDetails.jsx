9ff2f48b17f3d6a58515b06021f67691648b1a3c
aldobarrios81 <abv.mx81@gmail.com>
2022-09-05 20:36:56 -0500

    Desafío: Detalle de Producto

--- src/componentes/ItemDetails.jsx
+++ src/componentes/ItemDetails.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
+import ItemCount from './ItemCount'
 
 const Producto = () => {
     const [producto, setProductoCard] = useState([]);
@@ -20,16 +21,16 @@ const Producto = () => {
          <div className="card mb-3" style={{maxWidth: '540px'}}>
             <div className="row g-0">
                 <div className="col-md-4">
-                    <img src={"../img/"+ producto.img} className="img-fluid rounded-start" alt="..." />
+                    <img src={"../img/"+ producto.image} className="img-fluid rounded-start" alt="..." />
                 </div>
             <div className="col-md-8">
                 <div className="card-body">
-                    <h5 className="card-title">{producto.nombre}</h5>
+                    <h5 className="card-title">{producto.Categoria}</h5>
                     <p className="card-text">Marca: {producto.marca} </p>
-                    <p className="card-text">Modelo: {producto.modelo} </p>
+                    <p className="card-text">Modelo: {producto.stock} </p>
                     <p className="card-text">Precio: ${producto.precio} </p>
-                    <p className="card-text">Stock: ${producto.stock} </p>
-                    <button className='btn btn-dark'>Comprar</button>
+                    <ItemCount />
+                    <button className='btn btn-dark'>Add</button>
                 </div>
             </div>
         </div>
